1467ced4aac4d7a892aa0392d152c8449c22e7a7
ErikBoesen <me@erikboesen.com>
2022-04-19 16:43:16 -0400

    Finish with script for getting transactions back to certain date

--- pull.py
+++ pull.py
@@ -1,6 +1,7 @@
 from venmo_api import Client
 from getpass import getpass
 import json
+import datetime
 import unidecode
 
 """
@@ -16,11 +17,27 @@ client = Client(access_token=credentials['access_token'])
 
 
 me = client.user.get_my_profile()
-transactions = client.user.get_user_transactions(user_id=me.id)
+start_timestamp = int(datetime.datetime(2022, 4, 1).timestamp())
 amounts = {}
-while transactions:
-    for transaction in transactions:
-        print(transaction.amount)
-        choice = transaction.note.lower()
-        print(choice)
-    transactions = transactions.get_next_page()
+
+def get_relevant_transactions():
+    done = False
+    relevant_transactions = []
+    transactions = client.user.get_user_transactions(user_id=me.id)
+    while transactions:
+        for transaction in transactions:
+            print(transaction.amount)
+            choice = transaction.note.lower()
+            print(choice)
+            print(transaction.date_completed)
+            if transaction.date_completed < start_timestamp:
+                done = True
+                break
+            relevant_transactions.append(transaction)
+        if done:
+            break
+        transactions = transactions.get_next_page()
+    return relevant_transactions
+
+relevant_transactions = get_relevant_transactions()
+
