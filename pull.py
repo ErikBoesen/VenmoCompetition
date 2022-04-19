22b7f4ed9307143a428c5e10e155f34570a5dd0e
ErikBoesen <me@erikboesen.com>
2022-04-19 16:27:34 -0400

    Get my transactions

--- pull.py
+++ pull.py
@@ -1,25 +1,20 @@
 from venmo_api import Client
 from getpass import getpass
+import json
 
 """
 access_token = Client.get_access_token(username=input('Username: '),
                                         password=getpass())
 print("My token:", access_token)
 """
+with open('credentials.json', 'r') as f:
+    credentials = json.load(f)
 
 # Initialize api client using an access-token
-client = Client(access_token=access_token)
+client = Client(access_token=credentials['access_token'])
 
-# Search for users. You get a maximum of 50 results per request.
-users = client.user.search_for_users(query="Peter")
-for user in users:
-   print(user.username)
 
-# Or pass a callback to make it multi-threaded
-def callback(users):
-   for user in users:
-       print(user.username)
-
-client.user.search_for_users(query="peter",
-                            callback=callback,
-                            limit=10)
+me = client.user.get_my_profile()
+transactions = client.user.get_user_transactions(user_id=me.id)
+for transaction in transactions:
+    print(dir(transaction))
